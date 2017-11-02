8a5e7b09d51a14d7d8e9bc8a45802aacd8278e76
Zhang Yu <feiyuw@gmail.com>
2017-11-02 14:20:18 +0800

    fix io error

--- process/60_proc.py
+++ process/60_proc.py
@@ -13,6 +13,7 @@ TYPE_GAUGE = 'GAUGE'
 IGNORED_PPID = (1, 2)
 
 _PID_PTN = re.compile(r'^\d+$')
+_PNAME_PREFIX = 'ngc_'
 
 class ProcessMonitor(object):
     def run(self):
@@ -31,17 +32,20 @@ class ProcessMonitor(object):
             if not _PID_PTN.match(pid) or not os.path.exists('/proc/%s/status' % pid):
                 continue
             proc_name, fd_size = None, None
-            with open('/proc/%s/status' % pid) as fp:
-                for line in fp:
-                    if line.startswith('PPid:'):
-                        ppid = int(line.split(':')[1].strip())
-                        if ppid in IGNORED_PPID:
-                            continue
-                    if line.startswith('Name:'):
-                        proc_name = line.split(':')[1].strip()
-                    if line.startswith('FDSize:'):
-                        fd_size = int(line.split(':')[1].strip())
-            if proc_name is not None and fd_size is not None:
+            try:
+                with open('/proc/%s/status' % pid) as fp:
+                    for line in fp:
+                        if line.startswith('PPid:'):
+                            ppid = int(line.split(':')[1].strip())
+                            if ppid in IGNORED_PPID:
+                                continue
+                        if line.startswith('Name:'):
+                            proc_name = line.split(':')[1].strip()
+                        if line.startswith('FDSize:'):
+                            fd_size = int(line.split(':')[1].strip())
+            except IOError:
+                continue
+            if proc_name is not None and fd_size is not None and proc_name.startswith(_PNAME_PREFIX):
                 groups[proc_name] = groups.get(proc_name, 0) + fd_size
 
         for proc_name, fd_size in groups.iteritems():
